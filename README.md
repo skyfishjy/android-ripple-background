f7c619dab749ac1fcc574fe9b87e2f3fe406fd7e
JasonYu <skyfish.jy@gmail.com>
2014-11-07 11:55:50 +0800

    init commit

--- README.md
+++ README.md
@@ -1,4 +1,48 @@
-android-ripple-background
-=========================
+# Android Ripple Background
 
-A beautiful ripple animation for your app
+A beautiful ripple animation for your app. You can easily change its color, speed of wave, one ripple or multiple ripples. See demo below.
+
+![Simple Ripple](previews/rippleSimple.gif)　　　　　　![Multiple ripples](previews/rippleFoundDevice.gif)
+
+##Usage
+
+###Step 1
+
+####Install with Gradle
+
+//TODO push to maven center
+
+###Step 2
+####RippleBackground
+
+Add `RippleBackground` to your layout with content you want, like an ImageView. Configure the view customization elements using styleable attributes.
+ 
+```xml
+<org.yyu.library.RippleBackground
+    xmlns:android="http://schemas.android.com/apk/res/android"
+    xmlns:app="http://schemas.android.com/apk/res-auto"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:id="@+id/content"
+    app:rb_color="#0099CC"
+    app:rb_radius="32dp"
+    app:rb_rippleAmount="4"
+    app:rb_duration="3000"
+    app:rb_scale="6">
+    <ImageView
+        android:layout_width="64dp"
+        android:layout_height="64dp"
+        android:layout_centerInParent="true"
+        android:id="@+id/centerImage"
+        android:src="@drawable/demoImage"/>
+</org.yyu.library.RippleBackground>
+```
+
+##Theming
+* app:rb_color [color def:@android:color/holo_blue_dark] --> Color of the ripple
+* app:rb_radius [dimension def:64dp ] --> Radius of the ripple
+* app:rb_duration [integer def:3000 ] --> Duration of one ripple animation (millisecond) 
+* app:rb_rippleAmount [integer def:6] --> Max amount of ripples at one screen
+* app:rb_scale [interger def:6] --> Scale of ripple at the end of one animation cycle
+* app:rb_type [enum (fillRipple, strokeRipple) def:fillRipple] --> Filled circle or ring
+* app:rb_strokeWidth [dimension def:2dp] --> Stroke width of the ripple, ONLY work when rb_type="strokeRipple"
---
+++ sample/src/main/java/com/skyfishjy/ripplebackground/sample/MainActivity.java
@@ -0,0 +1,61 @@
+package com.skyfishjy.ripplebackground.sample;
+
+import java.util.ArrayList;
+import android.animation.Animator;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.ImageView;
+
+import com.skyfishjy.library.RippleBackground;
+import com.skyfishjy.library.ripplebackground.sample.R;
+
+
+public class MainActivity extends Activity {
+
+    private ImageView foundDevice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final RippleBackground rippleBackground=(RippleBackground)findViewById(R.id.content);
+
+        final Handler handler=new Handler();
+
+        foundDevice=(ImageView)findViewById(R.id.foundDevice);
+        ImageView button=(ImageView)findViewById(R.id.centerImage);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                rippleBackground.startAnimation();
+
+                handler.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        foundDevice();
+                    }
+                },3000);
+            }
+        });
+    }
+
+    private void foundDevice(){
+        AnimatorSet animatorSet = new AnimatorSet();
+        animatorSet.setDuration(400);
+        animatorSet.setInterpolator(new AccelerateDecelerateInterpolator());
+        ArrayList<Animator> animatorList=new ArrayList<Animator>();
+        ObjectAnimator scaleXAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleX", 0f, 1.2f, 1f);
+        animatorList.add(scaleXAnimator);
+        ObjectAnimator scaleYAnimator = ObjectAnimator.ofFloat(foundDevice, "ScaleY", 0f, 1.2f, 1f);
+        animatorList.add(scaleYAnimator);
+        animatorSet.playTogether(animatorList);
+        foundDevice.setVisibility(View.VISIBLE);
+        animatorSet.start();
+    }
+}
