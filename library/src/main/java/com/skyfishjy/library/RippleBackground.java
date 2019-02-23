b6532b57ee1de6765942442cca6558d03f3223f4
imanx <Imannamix@gmail.com>
2019-02-23 11:43:28 +0330

    add jcenter

--- library/src/main/java/com/skyfishjy/library/RippleBackground.java
+++ library/src/main/java/com/skyfishjy/library/RippleBackground.java
@@ -8,7 +8,6 @@ import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.RelativeLayout;
